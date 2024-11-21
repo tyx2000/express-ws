dab23c7a5f27d4b26cff84347f9c7107ce56b9e8
tyx2000 <154293941+tyx2000@users.noreply.github.com>
2024-11-21 17:55:59 +0800

    feat: insonmia

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
 const express = require('express');
 const Redis = require('ioredis');
 const cors = require('cors');
-
+const bcrypt = require('bcrypt');
 const http = require('http');
 const socketIO = require('socket.io');
+const jwt = require('jsonwebtoken');
 
 const redis = new Redis({
   host: 'localhost',
   port: 6379,
 });
 
-const verifyUid = (req, res, next) => {
-  const token = req.headers.authorization;
-  console.log(token);
-  next();
+const jwtSecretKey = '23434985894';
+
+const verifyToken = (req, res, next) => {
+  // Skip verification for login route
+  if (req.path === '/login') {
+    return next();
+  }
+
+  const token = req.headers.authorization.split(' ')[1];
+
+  console.log('middle', token);
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: 'No token provided',
+    });
+  } else {
+    jwt.verify(token, jwtSecretKey, (err, user) => {
+      if (err) {
+        return res.json({ data: 'invalid token' });
+      } else {
+        console.log('uuuuuuuuu', user);
+        next();
+      }
+    });
+  }
 };
 
 const app = express();
@@ -41,23 +65,42 @@ io.on('connection', (socket) => {
 });
 
 app.use(cors());
-app.use(verifyUid);
+app.use(verifyToken);
 
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.json({ data: 'hello' });
+app.get('/login', async (req, res) => {
+  const { username } = req.query;
+  console.log(req.ip, username);
+
+  const token = jwt.sign(
+    {
+      username,
+      loginAt: Date.now(),
+    },
+    jwtSecretKey,
+    {
+      expiresIn: '30d',
+    },
+  );
+
+  console.log('login', token);
+
+  redis.setex(`token:${username}`, 60 * 60 * 24 * 30, token);
+
+  res.json({ code: 200, success: true, data: { token } });
 });
 
-app.get('/login', (req, res) => {
-  const { uid } = req.query;
-  redis.setex('token-' + uid, 3600, uid);
-  res.json({ code: 200, success: true });
-});
 app.get('/logout', (req, res) => {
-  const { uid } = req.query;
-  redis.delete('token-' + uid);
-  res.json({ data: 'logout' });
+  const { username } = req.query;
+  redis.delete('token:' + username);
+  res.json({ success: true });
+});
+
+app.get('/search', async (req, res) => {
+  const { username } = req.query;
+  const user = await redis.get('token:' + username);
+  res.json({ success: !!res });
 });
 
 app.get('/chat', (req, res) => {
